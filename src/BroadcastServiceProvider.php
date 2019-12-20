169d9f9e8ccb42ec96ff8d9209c0e4129b5b4c54
reza <raftx24@gmail.com>
2019-12-20 12:53:15 +0330

    upgrades to php7.4

--- src/BroadcastServiceProvider.php
+++ src/BroadcastServiceProvider.php
@@ -9,12 +9,10 @@ class BroadcastServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Broadcast::channel('operations.{userId}', function ($user, $operationId) {
-            return (int) $user->id === (int) $operationId;
-        });
+        Broadcast::channel('operations.{userId}', fn($user, $operationId) => (
+            (int) $user->id === (int) $operationId
+        ));
 
-        Broadcast::channel('operations', function ($user) {
-            return $user->isAdmin() || $user->isSupervisor();
-        });
+        Broadcast::channel('operations', fn($user) => $user->isAdmin() || $user->isSupervisor());
     }
 }
