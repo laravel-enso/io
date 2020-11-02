bf61c3dd1b5097dcf4810c697f7128021f8fbb2c
reza <raftx24@gmail.com>
2020-11-02 15:41:27 +0330

    uses WebsocketServiceProvider

---
+++ src/WebsocketServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LaravelEnso\IO;
+
+use Illuminate\Support\Facades\App;
+use LaravelEnso\Core\Facades\Websockets;
+use LaravelEnso\Core\Models\User;
+use LaravelEnso\Core\WebsocketServiceProvider as CoreServiceProvider;
+use LaravelEnso\Roles\Enums\Roles;
+
+class WebsocketServiceProvider extends CoreServiceProvider
+{
+    public function boot()
+    {
+        $roles = App::make(Roles::class);
+
+        Websockets::register([
+            'io' => fn (User $user) => in_array($user->role_id, [$roles::Admin, $roles::Supervisor])
+                ? 'operations'
+                : 'operations.'.$user->id
+        ]);
+    }
+}
