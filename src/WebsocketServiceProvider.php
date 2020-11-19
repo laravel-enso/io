d7a16615f6ec7da45d67063b9e0cea2a346f06af
Adrian Ocneanu <aocneanu@users.noreply.github.com>
2020-11-19 12:04:18 +0000

    Apply fixes from StyleCI

--- src/WebsocketServiceProvider.php
+++ src/WebsocketServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace LaravelEnso\IO;
 
-use Illuminate\Support\Facades\App;
 use LaravelEnso\Core\Facades\Websockets;
 use LaravelEnso\Core\Models\User;
 use LaravelEnso\Core\WebsocketServiceProvider as CoreServiceProvider;
-use LaravelEnso\Roles\Enums\Roles;
 
 class WebsocketServiceProvider extends CoreServiceProvider
 {
