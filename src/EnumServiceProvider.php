a33ca0c5960407641db4028d405bebb85fef4677
ALIN <=>
2025-04-17 14:02:09 +0300

    upgraded to native enums

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -3,13 +3,8 @@
 namespace LaravelEnso\IO;
 
 use LaravelEnso\Enums\EnumServiceProvider as ServiceProvider;
-use LaravelEnso\IO\Enums\IOStatuses;
-use LaravelEnso\IO\Enums\IOTypes;
 
 class EnumServiceProvider extends ServiceProvider
 {
-    public $register = [
-        'ioStatuses' => IOStatuses::class,
-        'ioTypes' => IOTypes::class,
-    ];
+    public $register = [];
 }
